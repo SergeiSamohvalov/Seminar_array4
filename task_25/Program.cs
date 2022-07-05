4263735c9279fa499880a552ed57503ed0af09b7
sergei sam <ssi1970@mail.ru>
2022-07-05 18:12:33 +0700

    задача 25

---
+++ task_25/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+namespace tack_25
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            void FillArray(int[] number)
+            {
+                int length = number.Length;
+                int index = 0;
+                while (index < length)
+                {
+                    number[index] = new Random().Next(1, 10);
+                    index++;
+                }
+            }
+
+            void Printarray(int[] mass)
+            {
+                int count = mass.Length;
+                int index = 0;
+                while (index < count)
+                {
+                    Console.Write($"{mass[index].ToString()} ");
+                    index++;
+                }
+            }
+
+            int[] array = new int[2];
+
+            FillArray(array);
+
+            Printarray(array);
+
+            Console.WriteLine();
+
+            Console.WriteLine(Math.Pow(array[0], array[1]));
+        }
+    }
+}
